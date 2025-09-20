42a3d65a8542c187ed50069c2dfbf1b3215edfa4
YelyzavetaTartar <lizatarua@gmail.com>
2025-09-20 20:42:36 +0300

    Update RandomNumberGenerator.h

--- Lab1number3/RandomNumberGenerator.h
+++ Lab1number3/RandomNumberGenerator.h
@@ -1,1 +1,25 @@
+#ifdef RANDOM_NUMBER_GENERATOR_H
+#define RANDOM_NUMBER_GENERATOR_H
 
+#include <vector>
+#include <map>
+#include <random>
+#include <numeric>
+#include <stdexcept>
+
+class RandomNumberGenerator{
+public:
+RandomNumberGenerator(const std::vector<int>& numbers, const std::vector<int>& frequencies);
+int operator()();
+
+int getTotalFrequency() const { return totalFrequency; }
+
+private:
+std::vector<int> numbers;
+std::vector<int> cumulativeFrequencies;
+int totalFrequency = 0;
+
+std::mt19937 rng;
+};
+
+#endif// RANDOM_NUMBER_GENERATOR_H
